7c09a06d832835a6b285227da4961984a0fd19c5
IsaiahTek <42502161+IsaiahTek@users.noreply.github.com>
2019-08-06 00:16:06 +0100

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import BsdevPaginator from './Paginator'
-export default {
-    install:function(Vue,iterable){
-        Vue.component('bsdev-paginator', BsdevPaginator);
-        Vue.use(BsdevPaginator)
-    }
-}
